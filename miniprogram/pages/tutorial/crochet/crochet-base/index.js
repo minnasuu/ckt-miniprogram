d73340739b70c7cda396c007e00b61b8de43fd3b
minnasu <minnasu@tencent.com>
2025-08-16 18:09:53 +0800

    perf: 教程基本完成

---
+++ miniprogram/pages/tutorial/crochet/crochet-base/index.js
@@ -0,0 +1,102 @@
+// pages/tutorial/adbout-wave/crochet-base/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list: [],
+    title: "",
+    selectedIndex: 0, // 当前选中的导航项索引
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    this.getData(options.index);
+  },
+
+  getData(index){
+      wx.request({
+        url: 'https://suminhan.cn/ckt/api/crochetCourseData.json',
+        success: (res) => {
+          const newIndex = index || 0;
+          this.setData({
+            title: res.data.data[newIndex].cap
+          })
+          const newData = res.data.data[newIndex].contentMenuList.map(item => {
+            return {
+              ...item,
+              imgList: item.imgList.map(detail => {
+                return {
+                  ...detail,
+                  img_src: detail.img_src ? detail.img_src.replace('./', 'https://suminhan.cn/ckt/') : ''
+                }
+              })
+            }
+          })
+          this.setData({
+            list: newData
+          })
+        }
+      })
+  },
+
+  // 点击左侧导航项
+  onMenuClick(e) {
+    const index = e.currentTarget.dataset.index;
+    this.setData({
+      selectedIndex: index
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
---
+++ miniprogram/pages/tutorial/crochet/index.js
@@ -0,0 +1,62 @@
+// pages/tutorial/crochet-basic/index.js
+Component({
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    list: [
+      {key: "1",
+      title:"钩针针法（初级）",
+      image:'/assets/images/tutorial/crochet-basic/1.png',
+      content:'基础针法是编织的基础，选择合适的线材可以提高编织的舒适度和效果。',
+      level:'⭐️⭐️⭐️',
+      link: '/pages/tutorial/crochet/crochet-base/index?index=0'
+    },
+    {
+      key: "2",
+      title:"钩针起针方法",
+      image:'/assets/images/tutorial/crochet-basic/1.png',
+      content:'基础针法是编织的基础，选择合适的线材可以提高编织的舒适度和效果。',
+      level:'⭐️⭐️⭐️',
+      link: '/pages/tutorial/crochet/crochet-base/index?index=1'
+    },
+    {
+      key: "3",
+      title:"钩针符号表",
+      image:'/assets/images/tutorial/crochet-basic/1.png',
+      content:'基础针法是编织的基础，选择合适的线材可以提高编织的舒适度和效果。',
+      level:'⭐️⭐️⭐️',
+      link: '/pages/tutorial/crochet/crochet-base/index?index=2'
+    },
+    {
+      key: "4",
+      title:"钩针案例（初级）",
+      image:'/assets/images/tutorial/crochet-basic/1.png',
+      content:'基础针法是编织的基础，选择合适的线材可以提高编织的舒适度和效果。',
+      level:'⭐️⭐️⭐️',
+    },
+    {
+      key: "5",
+      title:"钩针案例（进阶）",
+      image:'/assets/images/tutorial/crochet-basic/1.png',
+      content:'基础针法是编织的基础，选择合适的线材可以提高编织的舒适度和效果。',
+      level:'⭐️⭐️⭐️'
+    },
+    ]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
