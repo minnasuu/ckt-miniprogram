4e0db69069070aff104efefebaff051e7f53dcfc
suminhan <minhansu508@gmail.com>
2025-09-06 19:14:36 +0800

    perf: 图片双击放大预览

--- miniprogram/pages/tutorial/components/menu-content-page/index.js
+++ miniprogram/pages/tutorial/components/menu-content-page/index.js
@@ -21,6 +21,10 @@ Component({
       value: false
     }
   },
+  data: {
+    __lastTapTimeMs: 0,
+    __lastTapImgUrl: ''
+  },
   methods: {
     onMenuTap(e){
       const index = e.currentTarget.dataset.index;
@@ -31,6 +35,33 @@ Component({
     },
     onImageError(e){
       this.triggerEvent('imageerror', { imgIndex: e.currentTarget.dataset.imgindex });
+    },
+    onImageTap(e) {
+      const tappedImgUrl = e.currentTarget.dataset.imgurl;
+      const now = Date.now();
+      const last = this.data.__lastTapTimeMs || 0;
+      const lastUrl = this.data.__lastTapImgUrl || '';
+      const isSameImg = lastUrl === tappedImgUrl;
+      const isDouble = isSameImg && (now - last) < 300; // 300ms window
+
+      if (isDouble) {
+        const item = this.data.currentItem || {};
+        const list = item.imgList || item.detailList || [];
+        const urls = list
+          .map(d => d && (d.img_src || d.img))
+          .filter(u => !!u);
+        const current = tappedImgUrl;
+        if (urls && urls.length) {
+          wx.previewImage({
+            current,
+            urls
+          });
+        }
+        // reset to avoid triple tap chaining
+        this.setData({ __lastTapTimeMs: 0, __lastTapImgUrl: '' });
+      } else {
+        this.setData({ __lastTapTimeMs: now, __lastTapImgUrl: tappedImgUrl });
+      }
     }
   }
 });
